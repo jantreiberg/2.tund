1090259feacb67e3c931b354263ce0599352f795
jantrei <jantrei@greeny.cs.tlu.ee>
2016-09-14 08:54:56 +0300

    esimesed koodiread

---
+++ helloWorld.php
@@ -0,0 +1,14 @@
+<?php
+
+	$firstName = "Jan"; 
+	$lastName = "Treiberg";
+	
+	//echo trükib välja muutuja väärtuse
+	//echo $firstName;
+	//echo $lastName;
+	// tühik "" vahele; punkt lisab stringe
+	
+	echo $firstName." ".$lastName
+?>
+
+
